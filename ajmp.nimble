e8cfc63dc7bbe0311d53fda886761b89c122e89b
Arham Jain <arhamjain@gmail.com>
2021-06-18 22:21:30 -0400

    added ajmp basic setup

---
+++ ajmp.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "ajusa"
+description   = "A simple command line music player"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["ajmp"]
+
+
+# Dependencies
+
+requires "nim >= 1.4.4"
+requires "nimbass"
---
+++ src/ajmp.nim
@@ -0,0 +1,15 @@
+import strutils, os
+
+import nimbass/bass
+import nimbass/bass_fx
+
+echo BASS_Init(cint(-1), cast[DWORD](44100), cast[DWORD](0), nil, nil)
+var file = BASS_StreamCreateFile(cast[BOOL](false), "./music.mp3".cstring, 0, 0, BASS_Stream_Decode)
+file = BASS_FX_TempoCreate(file, BASS_FX_TEMPO_ALGO_CUBIC)
+discard BASS_ChannelSetAttribute(cast[DWORD](file), cast[DWORD](BASS_ATTRIB_TEMPO), 50.cfloat)
+discard BASS_ChannelPlay(file, cast[BOOL](false));
+while true:
+  sleep(1000)
+
+ # discard BASS_ChannelPlay(file, cast[BOOL](false))
+ # discard BASS_ChannelPause(file)
