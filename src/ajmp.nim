f73076c12b09a878f063c7d7ba8d70aecbe1fd43
Arham Jain <arhamjain@gmail.com>
2021-06-18 22:42:16 -0400

    updated gitignore

--- src/ajmp.nim
+++ src/ajmp.nim
@@ -3,6 +3,8 @@ import strutils, os
 import nimbass/bass
 import nimbass/bass_fx
 
+echo "Starting"
+
 echo BASS_Init(cint(-1), cast[DWORD](44100), cast[DWORD](0), nil, nil)
 var file = BASS_StreamCreateFile(cast[BOOL](false), "./music.mp3".cstring, 0, 0, BASS_Stream_Decode)
 file = BASS_FX_TempoCreate(file, BASS_FX_TEMPO_ALGO_CUBIC)
