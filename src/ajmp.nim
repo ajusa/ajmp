8030c38d3a5c6841112d68fdcba603ee0ff7a457
Arham Jain <arhamjain@gmail.com>
2021-06-20 15:01:47 -0400

    added a skip feature

--- src/ajmp.nim
+++ src/ajmp.nim
@@ -10,8 +10,16 @@ var file = BASS_StreamCreateFile(cast[BOOL](false), "./music.mp3".cstring, 0, 0,
 file = BASS_FX_TempoCreate(file, BASS_FX_TEMPO_ALGO_CUBIC)
 discard BASS_ChannelSetAttribute(cast[DWORD](file), cast[DWORD](BASS_ATTRIB_TEMPO), 50.cfloat)
 discard BASS_ChannelPlay(file, cast[BOOL](false));
+proc skipPastSilence(file: HSTREAM) =
+  var volume = cast[int16](BASS_ChannelGetLevel(file))
+  while volume < 100:
+    var skipAmount = BASS_ChannelSeconds2Bytes(file, 0.02)
+    discard BASS_ChannelSetPosition(file, skipAmount, BASS_POS_RELATIVE)
+    volume = cast[int16](BASS_ChannelGetLevel(file))
+
+file.skipPastSilence()
 while true:
   sleep(1000)
 
- # discard BASS_ChannelPlay(file, cast[BOOL](false))
- # discard BASS_ChannelPause(file)
+# discard BASS_ChannelPlay(file, cast[BOOL](false))
+# discard BASS_ChannelPause(file)
